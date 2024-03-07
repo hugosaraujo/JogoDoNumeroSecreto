8d4fa183ff34f019e2bf646cc7373701da37a31d
hugo de araujo <hugo.saraujo@tutanota.com>
2024-03-07 11:27:51 -0300

    Aprimorando a lógica do jogo, dando mais informções para o usuário

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 alert('Boas vindas ao jogo de adivinhação!');
-let numeroDaSorte = 4; 
+
+let numeroSecreto = 4; 
+console.log(numeroSecreto);
 let palpite = prompt('Digite um número entre 0 e 10');
+
 if (palpite == numeroDaSorte) {
-    console.log('Parabéns, você acertou!');
+    alert('Parabéns, você acertou! o número secreto é: ' + numeroSecreto);
 } else {
-    console.log('Que pena, tente novamente!');
+    let maior = numeroSecreto > palpite;
+    
+    if (maior) {
+        alert(`O número secreto é maior que ${palpite}`);
+    } else {
+        alert(`O número secreto é menor que ${palpite}`);
+    }
 }
