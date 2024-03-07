e28460060c741c5519e98adebaaa879af1762cf0
hugo de araujo <hugo.saraujo@tutanota.com>
2024-03-07 10:56:16 -0300

    Criação da lógica do jogo do número secreto

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 alert('Boas vindas ao jogo de adivinhação!');
+let numeroDaSorte = 4; 
+let palpite = prompt('Digite um número entre 0 e 10');
+if (palpite == numeroDaSorte) {
+    console.log('Parabéns, você acertou!');
+} else {
+    console.log('Que pena, tente novamente!');
+}
