1930b018c9a9a428699cc506658bc0d821e98c08
hugo de araujo <hugo.saraujo@tutanota.com>
2024-03-08 20:42:19 -0300

    Adição de informações na tela e inserindo a lógica do programa em funções

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
-alert('Boas vindas ao jogo de adivinhação!');
+let verificarChute = () => {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
 
-let numeroLimite = 10;
-let numeroSecreto = Math.floor(Math.random() * numeroLimite + 1); 
-console.log(numeroSecreto);
-let palpite; 
-let tentativas = 0;
+let exibirTexto = (tag, texto) => {
+    let mensagem = document.querySelector(tag);
+    mensagem.innerHTML = texto;
+}
 
-while(palpite !== numeroSecreto) {
-    tentativas++;
-    palpite = parseInt(prompt(`Digite um número entre 0 e ${numeroLimite}:`));
-    
-    if (palpite == numeroSecreto) {
-        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
-        alert(`Parabéns, você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
-        break;
+let sorteiaNumero = (numeroMaximo) => {
+    return parseInt(Math.random() * numeroMaximo + 1);
+}
 
-    } else {
-        let maior = numeroSecreto > palpite;
+let numeroMaximo = 10;
+
+exibirTexto('h1', 'Jogo do Número Secreto');
+exibirTexto('p', `Tente adivinhar o número secreto entre 0 e ${numeroMaximo}`);
+
+let numeroSecreto = 10;
+
+
+
+
+
+/*
+let main = () => {
+    alert('Boas vindas ao jogo de adivinhação!');
+
+    let numeroLimite = 10;
+    let numeroSecreto = Math.floor(Math.random() * numeroLimite + 1); 
+    console.log(numeroSecreto);
+    let palpite; 
+    let tentativas = 0;
+
+    while(palpite !== numeroSecreto) {
+        tentativas++;
+        palpite = parseInt(prompt(`Digite um número entre 0 e ${numeroLimite}:`));
         
-        if (maior) {
-            alert(`O número secreto é maior que ${palpite}`);
+        if (palpite == numeroSecreto) {
+            let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+            alert(`Parabéns, você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+            break;
 
         } else {
-            alert(`O número secreto é menor que ${palpite}`);
+            let maior = numeroSecreto > palpite;
+            
+            if (maior) {
+                alert(`O número secreto é maior que ${palpite}`);
 
+            } else {
+                alert(`O número secreto é menor que ${palpite}`);
+
+            }
         }
     }
 }
+*/
